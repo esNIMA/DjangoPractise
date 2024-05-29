fa80b76ac79b030f4865e40e3689efe584292670
esNIMA <s.nima.mahmoodian@gmail.com>
2024-05-29 18:27:36 +0330

    I love Django

--- 1-Firstproject/shop/courses_app/urls.py
+++ 1-Firstproject/shop/courses_app/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('courses' , views.courses),
+    path('detail/<int:id>' , views.course_detail)
 ]
--- 1-Firstproject/shop/courses_app/views.py
+++ 1-Firstproject/shop/courses_app/views.py
@@ -6,3 +6,8 @@ from .models import Course
 def courses(request):
     courses = Course.objects.all()
     return render(request, "courses_app/courses.html", {"courses": courses})
+
+
+def course_detail(request, id): 
+    course = Course.objects.get(id= id)
+    return render(request, "courses_app/course_detail.html", {"course" : course})
